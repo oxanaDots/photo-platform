d07348f6728d6acffa5a51378a973607785adb73
Oksana Dotsenko <oksanadotsenko967@gmail.com>
2025-06-22 15:26:01 +0100

    new

---
+++ src/routes/index.ts
@@ -0,0 +1,11 @@
+import { createBrowserRouter } from "react-router-dom";
+import Home from "../Home/Home";
+const routes= createBrowserRouter([
+    {
+        path: '/',
+        element: Home(),
+    },
+    {
+        
+    }
+])
