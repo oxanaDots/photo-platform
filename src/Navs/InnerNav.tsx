4416bc76b7a8c074e83970723ba27cac5c9b3fa9
Oksana Dotsenko <oksanadotsenko967@gmail.com>
2025-06-27 23:44:09 +0100

    new

--- src/Navs/InnerNav.tsx
+++ src/Navs/InnerNav.tsx
@@ -5,9 +5,9 @@ const navItems = {'Make a Booking': '/newbooking', 'Sign Out': '/signout'}
 
 const InnerNav = () => {
   return (
-   <nav className='flex justify-between items-center w-full'>
-         <h2 className='p-4 font-bold text-orange-500'>Logo</h2>
-    <ul className='flex gap-3 p-4 justify-end'>
+   <nav className='flex justify-between items-center w-[80%]'>
+         <h2 className='py-4 font-bold text-orange-500'>Logo</h2>
+    <ul className='flex gap-3  justify-end'>
         {Object.entries(navItems).map(([item, path]) => <NavLink  to={path}><li className={`px-4 text-[0.8rem] py-[0.3rem] rounded-3xl   ${ item !== 'Sign Up'? 'bg-indigo-800 bg-opacity-10 text-violet-950' : 'bg-violet-950 text-orange-50 '}`}>{item}</li></NavLink>)}
     </ul>
    
